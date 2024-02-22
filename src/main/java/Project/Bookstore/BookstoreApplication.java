bf9a8d80a56e697e3bbd7898aba3a28834f0497e
jonname <146915685+jonname@users.noreply.github.com>
2024-02-22 15:27:53 +0200

    first commit

--- src/main/java/Project/Bookstore/BookstoreApplication.java
+++ src/main/java/Project/Bookstore/BookstoreApplication.java
@@ -1,7 +1,11 @@
 package Project.Bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import Project.Bookstore.domain.Book;
+import Project.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -9,5 +13,16 @@ public class BookstoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+@Bean
+public CommandLineRunner demo(BookRepository repository) {
+	return (args) -> {
+		Book b1 = new Book("Eka kirja", "Eka kirjailija", 2024, "123456", 19.90);
+		Book b2 = new Book("Toka kirja", "Toka kirjailija", 2020, "124554", 15.90);
+		Book b3 = new Book("Kolmas kirja", "Kolmas kirjailija", 2013, "123236", 10.00);
 
+		repository.save(b1);
+		repository.save(b2);
+		repository.save(b3);
+	};
+	}
 }
--- src/main/java/Project/Bookstore/domain/Book.java
+++ src/main/java/Project/Bookstore/domain/Book.java
@@ -1,12 +1,39 @@
 package Project.Bookstore.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+private Long id;
 private String title;
 private String author;
 private int publicationYear;
 private String isbn; 
 private double price;
+
+public Book() {}
+
+public Book(String title, String author, int publicationYear, String isbn, double price) {
+    this.title = title;
+    this.author = author;
+    this.publicationYear = publicationYear;
+    this.isbn = isbn;
+    this.price = price;
+}
+
+
+public Long getId() {
+    return id;
+}
+public void setId(Long id) {
+    this.id = id;
+}
 public String getTitle() {
     return title;
 }
